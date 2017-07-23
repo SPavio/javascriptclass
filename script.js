4f86ac6edce0f69efde149f28ac38140008cd48b
Scott Pavio <scottpavio@gmail.com>
2017-07-23 15:19:56 -0400

    JS class work up to 7/23/17

---
+++ script.js
@@ -0,0 +1,86 @@
+// Lecture: Variables
+
+var name = 'Scott';
+console.log(name);
+
+var lastName = 'Pavio';
+console.log(lastName);
+
+var age = 41;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+
+// Lecture: Variables 2
+
+var name = 'Scott';
+var age = 41;
+//console.log(name + age);
+
+var job, isMarried;
+
+//console.log(job);
+job = 'technician';
+isMarried = true;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+age = 'forty-one';
+job = 'programmer';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is your last name?');
+console.log(lastName);
+
+
+// Lecture Operators
+var now = 2017;
+var age = 41;
+var birthYear = now - age;
+var birthYear = now - age * 2;
+
+
+console.log(birthYear);
+
+
+// Lecture if/else statements
+
+
+var name = 'John';
+var age = '26';
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+} else {
+    console.log(name + ' will hopefully marry soon.');
+}
+
+isMarried = false;
+
+if(isMarried) {
+    consol.log('Yes!');
+} else {
+    console.log('No!');
+}
+
+
+
+//Lecture Boolean logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
